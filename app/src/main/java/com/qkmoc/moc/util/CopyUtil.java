db4622c0e644bd85873216a137e10c3582cd6443
HugeTerry <xuxt-314@qq.com>
2016-07-30 08:29:21 +0800

    新增复制到剪贴板工具类

---
+++ app/src/main/java/com/qkmoc/moc/util/CopyUtil.java
@@ -0,0 +1,24 @@
+package com.qkmoc.moc.util;
+
+import android.annotation.TargetApi;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.os.Build;
+import android.widget.Toast;
+
+/**
+ * Created by hugeterry(http://hugeterry.cn)
+ * Date: 16/7/30 08:27
+ */
+public class CopyUtil {
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static void copyToClipBoard(Context context, String text) {
+        ClipboardManager cm = (ClipboardManager) context.getSystemService(
+                Context.CLIPBOARD_SERVICE);
+        cm.setPrimaryClip(ClipData.newPlainText("coderfun_copy", text));
+        Toast.makeText(context, "复制成功", Toast.LENGTH_SHORT).show();
+    }
+
+}
