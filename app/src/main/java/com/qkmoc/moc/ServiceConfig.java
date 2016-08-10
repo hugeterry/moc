21dfa6493905a73c852751d16b3b30f80f04f7e8
HugeTerry <xuxt-314@qq.com>
2016-08-10 15:49:58 +0800

    增加公共设置类

---
+++ app/src/main/java/com/qkmoc/moc/ServiceConfig.java
@@ -0,0 +1,9 @@
+package com.qkmoc.moc;
+
+/**
+ * Created by hugeterry(http://hugeterry.cn)
+ * Date: 16/8/10 12:46
+ */
+public class ServiceConfig {
+    public static boolean socketFirstStart = true;
+}
